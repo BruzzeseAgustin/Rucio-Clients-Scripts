d0062324cbb681b21d1d3d2ce98226e80f4cb974
BruzzeseA <bruzzese.agustin@gmail.com>
2021-02-07 12:02:57 +0100

    Update set_up_virtual_env.sh

--- set_up_virtual_env.sh
+++ set_up_virtual_env.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
+# FYI please adress to the following link if 
+# you want to install further dependencies in jupyter.pic
+
 apt install python3-pip
 
 pip install virtualenv
-virtualenv rucio
+python3 -m venv rucio
+# virtualenv rucio
+
 source rucio/bin/activate
 
 git clone https://github.com/BruzzeseAgustin/Rucio-Clients-Scripts.git
@@ -12,3 +17,4 @@ pip install -r Rucio-Clients-Scripts/requirements.txt
 cp Rucio-Clients-Scripts/rucio.cfg rucio/etc
 
 export RUCIO_HOME=`pwd`/rucio/
+
